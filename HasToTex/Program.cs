a1a46a1ed15f9d93a605dbc867ffd67a68defbe3
MetaColon <timo@teemze.de>
2020-08-08 12:40:21 +0200

    Move Readme

--- HasToTex/Program.cs
+++ HasToTex/Program.cs
@@ -1,12 +1,13 @@
 ﻿using System;
 
+
 namespace HasToTex
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main (string [] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine ("Hello World!");
         }
     }
 }
