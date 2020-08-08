f295e5e36aec585764e235ed35faf6871773d696
MetaColon <timo@teemze.de>
2020-08-08 12:39:05 +0200

    Initial commit

---
+++ HasToTex/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HasToTex
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
